5dfac6376212ba6344ae98a0d532d271c82b6e2e
orhan <orhanrizayildirim@gmail.com>
2022-04-05 18:16:34 -0400

    Added more work

--- src/work1/Hello.java
+++ src/work1/Hello.java
@@ -4,6 +4,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Created Git Repository");
+        System.out.println("Made few commits");
 
     }
 }
