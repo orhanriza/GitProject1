30ca9f5d6c803a2a0d0c5a2091c3607261cb235b
orhan <orhanrizayildirim@gmail.com>
2022-07-19 15:43:43 +0300

    initial commit 1

--- src/work1/Hello.java
+++ src/work1/Hello.java
@@ -7,5 +7,6 @@ public class Hello {
         System.out.println("Made few commits");
         System.out.println("work1 is ready");
         System.out.println("i'll do this");
+        System.out.println("it's a new day");
     }
 }
